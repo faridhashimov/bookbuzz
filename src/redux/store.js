a1e6838d7d88270d89827ea5a0233434b131fd0e
Farid <fargmov@gmail.com>
2023-06-26 22:00:01 +0300

    add listener middleware for data persisting

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,31 @@ import {
     createListenerMiddleware,
     isAnyOf,
 } from '@reduxjs/toolkit'
-import useReducer, { logOut, userLogin } from './userSlice'
+import userReducer, { logOut, userLogin } from './userSlice'
+import cartReducer, {
+    addToCart,
+    decreaseQt,
+    deleteFromCart,
+    increaseQt,
+} from './cartSlice'
 
 import { bookbuzzApi } from './bookbuzzApi/bookbuzzApi'
+
+const cartListenerMiddleware = createListenerMiddleware()
+cartListenerMiddleware.startListening({
+    matcher: isAnyOf(
+        addToCart,
+        deleteFromCart,
+        increaseQt,
+        decreaseQt,
+    ),
+    effect: (action, listenerApi) => {
+        localStorage.setItem(
+            'products',
+            JSON.stringify(listenerApi.getState().cart)
+        )
+    },
+})
 
 const userListenerMiddleware = createListenerMiddleware()
 userListenerMiddleware.startListening({
@@ -18,19 +40,24 @@ userListenerMiddleware.startListening({
     },
 })
 
+const cartState = JSON.parse(localStorage.getItem('products') || '[]')
 const userState = JSON.parse(localStorage.getItem('userData') || '{}')
 
 export const store = configureStore({
     preloadedState: {
         user: userState,
+        cart: cartState,
     },
     reducer: {
-        user: useReducer,
+        user: userReducer,
+        cart: cartReducer,
         [bookbuzzApi.reducerPath]: bookbuzzApi.reducer,
     },
+    
     middleware: (getDefultMiddleware) =>
         getDefultMiddleware().concat(
             bookbuzzApi.middleware,
+            cartListenerMiddleware.middleware,
             userListenerMiddleware.middleware
         ),
 })
