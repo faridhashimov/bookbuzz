6319a022e596f6ce8e4ec6659fe4640947d4bb91
Farid <fargmov@gmail.com>
2023-06-26 22:01:24 +0300

    change headers

--- src/redux/bookbuzzApi/bookbuzzApi.js
+++ src/redux/bookbuzzApi/bookbuzzApi.js
@@ -4,17 +4,22 @@ export const bookbuzzApi = createApi({
     reducerPath: 'bookbuzzApi',
     tagTypes: ['Products', 'Orders'],
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://dry-falls-41861-88221b34a74e.herokuapp.com/http://bookbuzz.cronhex.com/api/v1/',
+        baseUrl:
+            'https://dry-falls-41861-88221b34a74e.herokuapp.com/http://bookbuzz.cronhex.com/api/v1/',
         mode: 'cors',
         credentials: 'same-origin',
         prepareHeaders: (headers, { getState }) => {
             // headers.set('Content-Type', 'application/json')
             // headers.set('Access-Control-Allow-Origin', '*,*')
             // headers.set('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT')
-            const token = getState().user?.result?.jwt
+            headers.set(
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With'
+            )
+            const token = getState().user?.user?.result?.jwt
 
             if (token) {
-                headers.set('token', `Bearer ${token}`)
+                headers.set('Token', `Bearer ${token}`)
             }
 
             return headers
@@ -24,6 +29,7 @@ export const bookbuzzApi = createApi({
         getAllProducts: build.query({
             query: () => 'product/explore',
             providesTags: (result, error, id) => [{ type: 'Products', id }],
+            transformResponse: (response) => response.result.products,
         }),
         getProduct: build.query({
             query: (id) => `product/details?productId=${id}`,
